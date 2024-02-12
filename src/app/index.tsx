3b44fc534c2bd7e5e395ddf77445e28b890cf288
gbflores <guilhermebflores@gmail.com>
2024-02-12 15:24:15 -0300

    chore: add tailwind to the project, and change folders directorys

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
 import { View, Text } from "react-native";
 export default function Home() {
     return (
-        <View>
-            <Text>Hello React Native</Text>
+        <View className="bg-slate-900 flex-1">
+            <Text className="text-white">Hello React Native!!</Text>
         </View>
     )
 }
