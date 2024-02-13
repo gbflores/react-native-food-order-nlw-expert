eda2139ef9a70b0fb89a9489e9aea5ef3ff8873b
gbflores <guilhermebflores@gmail.com>
2024-02-13 17:38:10 -0300

    feat: type of section list, add title to the product page and change in function handleprod

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from "react-native";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Button } from "@/components/button";
@@ -12,11 +12,17 @@ export default function Product() {
     const cartStore = useCartStore();
     const navigation = useNavigation();
 
-    const product = PRODUCTS.filter((item) => item.id == id)[0]
+    const product = PRODUCTS.find((item) => item.id == id)
 
     function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack()
+        if (product) {
+            cartStore.add(product)
+            navigation.goBack()
+        }
+    }
+
+    if (!product) {
+        return <Redirect href="/"></Redirect>
     }
 
     return (
@@ -24,6 +30,7 @@ export default function Product() {
             <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
             <LinkButton title="Voltar" href="/" className="absolute z-10 w-16 h-8 p-2 font-bold text-center text-black rounded-sm top-5 left-5 bg-lime-300" />
             <View className="flex-1 p-5 mt-8">
+                <Text className="text-xl text-white font-heading">{product.title}</Text>
                 <Text className="my-2 text-2xl text-lime-400 font-heading">
                     {formatCurrency(product.price)}
                 </Text>
