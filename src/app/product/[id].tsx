875b5dc75c41bee8c0aaad057e298a6a299fe723
gbflores <guilhermebflores@gmail.com>
2024-02-13 16:42:53 -0300

    feat: add buttons to back to menu, and add infos and buttons to the product page

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -2,6 +2,9 @@ import { Image, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
 
 export default function Product() {
     const { id } = useLocalSearchParams()
@@ -10,7 +13,7 @@ export default function Product() {
     return (
         <View className="flex-1">
             <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
-
+            <LinkButton title="Voltar" href="/" className="absolute z-10 w-16 h-8 p-2 font-bold text-center text-black rounded-sm top-5 left-5 bg-lime-300" />
             <View className="flex-1 p-5 mt-8">
                 <Text className="my-2 text-2xl text-lime-400 font-heading">
                     {formatCurrency(product.price)}
@@ -25,6 +28,15 @@ export default function Product() {
                         </Text>
                     ))
                 }
+            </View>
+            <View className="gap-5 p-5 pb-8">
+                <Button>
+                    <Button.Icon>
+                        <Feather name="plus-circle" size={20} />
+                    </Button.Icon>
+                    <Button.Text>Adicionar ao Pedido</Button.Text>
+                </Button>
+                <LinkButton className="text-base text-center text-slate-300 font-body" title="Voltar ao cardápio" href="/" />
             </View>
         </View>
     )
